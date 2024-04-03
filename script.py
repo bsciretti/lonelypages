d9492b18d094331623bff2ad1232849f3b80665e
Brian Sciretti <45018431+bsciretti@users.noreply.github.com>
2024-04-03 20:29:48 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,13 @@
-import pywikibot, time
+import pywikibot, time, argparse
 
 def get_lombard_articles():
     site = pywikibot.Site("lmo", "wikipedia")
     print("Scaricando elenco pagine")
-    return list(site.allpages(namespace=0, filterredir=False))
-
+    if start_page:
+        return [site.page(start_page)]
+    else:
+        return list(site.allpages(namespace=0, filterredir=False))
+        
 def check_links(article):
     if article.isRedirectPage() or article.isDisambig():
         return False
@@ -38,7 +41,9 @@ def main():
             add_template(article)
 
 if __name__ == "__main__":
-    main()
-#################
+    parser = argparse.ArgumentParser(description="Script per aggiungere il template 'O' a Wikipedia in lombardo.")
+    parser.add_argument("-start", nargs="?", help="Titolo della pagina da cui iniziare il controllo.", dest="start_page")
+    args = parser.parse_args()
 
+    main(args.start_page)
 
